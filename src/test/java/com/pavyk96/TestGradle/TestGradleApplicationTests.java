9332c0323cc90755efc8fe08179afb1777d79521
daniil <pepethefrog27@mail.ru>
2025-02-22 10:52:30 +0500

    Initial commit

---
+++ src/test/java/com/pavyk96/TestGradle/TestGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavyk96.TestGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
