7c357329ab17cb36e24b1cfb03ddc02d58a09c3c
daniil <pepethefrog27@mail.ru>
2025-03-25 13:04:46 +0500

    Добавил глобальный обработчик ошибок

---
+++ src/main/java/com/pavyk96/TestGradle/hw4/exeption/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.pavyk96.TestGradle.hw4.exeption;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new LinkedHashMap<>();
+        errors.put("status", "error");
+        errors.put("code", "400");
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
