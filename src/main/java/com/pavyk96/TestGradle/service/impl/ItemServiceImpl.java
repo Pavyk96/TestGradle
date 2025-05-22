5d8447b136cb8d9810c054db4ff292e2eaad01ec
daniil <pepethefrog27@mail.ru>
2025-05-22 16:08:32 +0500

    Добавил свобственную метрику + отказался от ручного вычисления времени выполнения метода и использовал @Timed

--- src/main/java/com/pavyk96/TestGradle/service/impl/ItemServiceImpl.java
+++ src/main/java/com/pavyk96/TestGradle/service/impl/ItemServiceImpl.java
@@ -1,46 +1,55 @@
 package com.pavyk96.TestGradle.service.impl;
 
 import com.pavyk96.TestGradle.service.ItemService;
+import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Timer;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
+
     private final MeterRegistry meterRegistry;
-    private Counter totalOrdersCounter;
+    private final Map<String, Counter> itemCounters = new ConcurrentHashMap<>();
     private Counter generalOrdersCounter;
-    private Timer executionTimer;
 
     @PostConstruct
     public void init() {
-        this.totalOrdersCounter = Counter.builder("orders.total")
-                .description("Total orders by item")
-                .register(meterRegistry);
-
         this.generalOrdersCounter = Counter.builder("orders.count")
                 .description("Total orders count")
-                .register(meterRegistry);
-
-        this.executionTimer = Timer.builder("orders.execution.time")
-                .description("Time to process getOrder")
-                .publishPercentiles(0.5, 0.95)
                 .register(meterRegistry);
     }
 
     @Override
+    @Timed(
+            value = "orders.execution.time",
+            description = "Time to process getOrder",
+            percentiles = {0.5, 0.95}
+    )
     public String createOrder(String itemName) {
-        return executionTimer.record(() -> {
-            generalOrdersCounter.increment();
-            totalOrdersCounter.increment();
+        generalOrdersCounter.increment();
 
-            return "Заказ " + itemName + " успешно создан!";
-        });
+        Counter itemCounter = itemCounters.computeIfAbsent(itemName, name ->
+                Counter.builder("orders.by.item")
+                        .description("Orders by specific item")
+                        .tag("item", name)
+                        .register(meterRegistry)
+        );
+
+        itemCounter.increment();
+
+        return "Заказ " + itemName + " успешно создан!";
+    }
+
+    public Map<String, Double> getOrdersByItem() {
+        Map<String, Double> result = new ConcurrentHashMap<>();
+        itemCounters.forEach((item, counter) -> result.put(item, counter.count()));
+        return result;
     }
 }
